67ba3da7c7cd3b4db3360d32913d2736e2128419
mateusz_kalwaj <tynozobacz@gmail.com>
2019-07-25 23:47:38 +0200

    initial commit

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    [TextArea(10, 14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates; //array stanów gry
+
+    public string GetStateStory() {
+        return storyText;
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+}
